4a62caf6eefb9e4d5c1cb19ca6dbf104ed940166
Acacio De Lima <limadeacacio@gmail.com>
2021-04-08 15:54:12 -0300

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,36 @@
-import React, { useState, useEffect } from "react";
-import ReactAudioPlayer from "react-audio-player";
+import React from "react";
 
-import bgLight from "../../assets/images/bgLight.png";
-import bgDark from "../../assets/images/bgDark.png";
-import scarySound from "../../assets/sounds/scary.mp3";
+import bgPoster from "../../assets/images/bgPoster.png";
+import bgVideo from "../../assets/video/bgVideo.mp4";
 
 import styles from "./styles.module.css";
 
 export default function Home() {
-  const [selectedImage, setSelectedImage] = useState(bgLight);
+  const handleVideoFullScreen = (event) => {
+    if (event) event.preventDefault();
 
-  useEffect(() => {
-    setInterval(() => {
-      const imageList = [bgLight, bgDark];
-      const imageRandom = imageList[Math.floor(Math.random() * imageList.length)];
-      setSelectedImage(imageRandom);
-    }, 10);
-  }, []);
+    const el = document.getElementById("video");
+    if (el.requestFullscreen) {
+      el.requestFullscreen();
+    } else if (el.mozRequestFullScreen) {
+      el.mozRequestFullScreen();
+    } else if (el.webkitRequestFullscreen) {
+      el.webkitRequestFullscreen();
+    } else if (el.msRequestFullscreen) {
+      el.msRequestFullscreen();
+    }
+  };
 
   return (
-    <>
-      <ReactAudioPlayer src={scarySound} autoPlay />
-      <div
-        className={styles.mainDiv}
-        style={{ backgroundImage: `url(${selectedImage})` }}
-      ></div>
-    </>
+    <div className={styles.mainDiv}>
+      <video
+        id="video"
+        onClick={handleVideoFullScreen}
+        src={bgVideo}
+        poster={bgPoster}
+        autoPlay
+        loop
+      ></video>
+    </div>
   );
 }
